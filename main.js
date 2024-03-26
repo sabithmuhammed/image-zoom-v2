3c8d44c6669ec3068c02676cdffee196b391d025
sabith <sabithmuhammed136@gmail.com>
2024-03-26 10:33:12 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const image = document.querySelector('.img-container')
+let x,y,width,height;
+image.addEventListener('mouseenter',()=>{
+    const size = image.getBoundingClientRect();
+    x = size.x
+    y = size.y
+    width = size.width
+    height = size.height
+})
+image.addEventListener('mousemove',e =>{
+    const horizontal = (e.clientX - x) / width*100
+    const vertical = (e.clientY - y) / height*100
+    image.style.setProperty('--x',horizontal+'%')
+    image.style.setProperty('--y',vertical+'%')
+    console.log(x,y,height,width);
+})
